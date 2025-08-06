e5b3b990a2054034d5605ada25ce1d46aa0c0296
Satendra Kumar <satyendrakumar.contact@gmail.com>
2025-08-06 15:35:57 +0530

    Image error fixed

--- src/components/core/Auth/Template.jsx
+++ src/components/core/Auth/Template.jsx
@@ -1,48 +1,37 @@
 import { FcGoogle } from "react-icons/fc";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 // import { toast } from "react-hot-toast";
 
-import frameImg from "../../../assets/Images/frame.png"
-import LoginForm from "./LoginForm"
-import SignupForm from "./SignupForm"
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 import { handleGoogleLogin } from "../../../services/operations/authFire";
-import '../../../styles/Login.css'
+import "../../../styles/Login.css";
 
 function Template({ title, description1, description2, image, formType }) {
-    const { loading } = useSelector((state) => state.auth)
+  const { loading } = useSelector((state) => state.auth);
 
-    return (
-        <div className="">
-            {/* {
+  return (
+    <div className="">
+      {/* {
                 loading ? (
                     <div className=""></div>
                 ) : ( */}
-                    <div className="">
-                        <div className="">
-                
-                            {formType === "signup" ? <SignupForm /> : 
-                            <LoginForm />
-                            }                          
-                                
-                                <p className="Or-design">
-                                    OR
-                                </p>
-                                
-                        
-                            <button className="SignUp-button" onClick={handleGoogleLogin}>
-                                <FcGoogle />
-                                <p>
-                                    Sign Up With Google 
-                                </p>
-                            </button>
-                        </div>
+      <div className="">
+        <div className="">
+          {formType === "signup" ? <SignupForm /> : <LoginForm />}
 
-                       
-                    </div>
-                {/* )
-            } */}
+          <p className="Or-design">OR</p>
+
+          <button className="SignUp-button" onClick={handleGoogleLogin}>
+            <FcGoogle />
+            <p>Sign Up With Google</p>
+          </button>
         </div>
-    )
+      </div>
+      {/* )
+            } */}
+    </div>
+  );
 }
 
-export default Template
+export default Template;
